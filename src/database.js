e62adfe67337135d1317a31c44f698691b003314
williamcadenas21 <williamcadenas21@gmail.com>
2018-11-12 20:33:55 -0500

    configuration of sequilize

--- src/database.js
+++ src/database.js
@@ -1,8 +1,41 @@
-const mysql = require('mysql');
-
+const Sequelize = require('sequelize');
 
 //url: mysql://b39610fbae5cd9:208daf58@
 //us-cdbr-iron-east-01.cleardb.net/heroku_3885dbc12e8e764?reconnect=true 
+
+//const sequelize = new Sequelize('mysql://b39610fbae5cd9:208daf58@us-cdbr-iron-east-01.cleardb.net/heroku_3885dbc12e8e764');
+
+//LOCAL
+// const sequelize = new Sequelize('carpooldb', 'root', null, {
+//   dialect: 'mysql',
+//   host: "localhost",
+//   port: 3306,
+// });
+
+// sequelize
+//   .authenticate()
+//   .then(() => {
+//     console.log('Connection has been established successfully.');
+//   })
+//   .catch(err => {
+//     console.error('Unable to connect to the database:', err);
+//   });
+
+
+//REMOTO
+const sequelize = new Sequelize('mysql://b39610fbae5cd9:208daf58@us-cdbr-iron-east-01.cleardb.net/heroku_3885dbc12e8e764');
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+module.exports = sequelize;
+
 // const mysqlConnection = mysql.createConnection({
 //   host: 'us-cdbr-iron-east-01.cleardb.net',
 //   user: 'b39610fbae5cd9', //Heroku MySql user 
@@ -20,39 +53,39 @@ const mysql = require('mysql');
 //   }
 // });
 
-const db_config = {
-  host: 'us-cdbr-iron-east-01.cleardb.net',
-  user: 'b39610fbae5cd9', //Heroku MySql user 
-  password: '208daf58',//si no hay contrasña debe estar en blanco
-  database: 'heroku_3885dbc12e8e764',
-  multipleStatements:true,
-}
+// const db_config = {
+//   host: 'us-cdbr-iron-east-01.cleardb.net',
+//   user: 'b39610fbae5cd9', //Heroku MySql user 
+//   password: '208daf58',//si no hay contrasña debe estar en blanco
+//   database: 'heroku_3885dbc12e8e764',
+//   multipleStatements:true,
+// }
 
-function handleDisconnect() {
-  mysqlConnection = mysql.createConnection(db_config); // Recreate the connection, since
-                                                  // the old one cannot be reused.
+// function handleDisconnect() {
+//   mysqlConnection = mysql.createConnection(db_config); // Recreate the connection, since
+//                                                   // the old one cannot be reused.
 
-  mysqlConnection.connect(function(err) {              // The server is either down
-    if(err) {                                     // or restarting (takes a while sometimes).
-      console.log('error when connecting to db:', err);
-      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-    } else {
-      console.log('db is connected');
-    }                                    // to avoid a hot loop, and to allow our node script to
-  });                                     // process asynchronous requests in the meantime.
-                                          // If you're also serving http, display a 503 error.
-  mysqlConnection.on('error', function(err) {
-    console.log('db error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-      handleDisconnect();                         // lost due to either server restart, or a
-    } else {
-     console.log('error is err:', err);                                      // connnection idle timeout (the wait_timeout
-     // throw err;                                  // server variable configures this)
-    }
-  });
-}
+//   mysqlConnection.connect(function(err) {              // The server is either down
+//     if(err) {                                     // or restarting (takes a while sometimes).
+//       console.log('error when connecting to db:', err);
+//       setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+//     } else {
+//       console.log('db is connected');
+//     }                                    // to avoid a hot loop, and to allow our node script to
+//   });                                     // process asynchronous requests in the meantime.
+//                                           // If you're also serving http, display a 503 error.
+//   mysqlConnection.on('error', function(err) {
+//     console.log('db error', err);
+//     if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+//       handleDisconnect();                         // lost due to either server restart, or a
+//     } else {
+//      console.log('error is err:', err);                                      // connnection idle timeout (the wait_timeout
+//      // throw err;                                  // server variable configures this)
+//     }
+//   });
+// }
 
-handleDisconnect();
-module.exports = mysqlConnection;
+// handleDisconnect();
+
 
 //archivo listo
--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -1,64 +1,130 @@
 const express = require('express');
 const router = express.Router();
 
-const mysqlConnection  = require('../database.js');
+//const mysqlConnection  = require('../database.js');
+const sequilize  = require('../database.js');
 
 router.get('/', (req, res) => {
   res.send('welcome to my rest api');
 });
 
 // GET all User
+// router.get('/users/getAll', (req, res) => {
+//   mysqlConnection.query('SELECT * FROM usuarios', (err, rows, fields) => {
+//     if(!err) {
+//       res.json(rows);
+//     } else {
+//       console.log(err);
+//     }
+//   });  
+// });
+
 router.get('/users/getAll', (req, res) => {
-  mysqlConnection.query('SELECT * FROM usuarios', (err, rows, fields) => {
-    if(!err) {
-      res.json(rows);
-    } else {
-      console.log(err);
-    }
-  });  
+  sequilize.query('SELECT * FROM usuarios').then( rows => {
+    //console.log(rows);
+    res.json(rows[0]);
+  });
 });
 
 // GET An User
+
+// router.get('/users/get/:email_id', (req, res) => {
+//   const { email_id } = req.params; 
+//   mysqlConnection.query('SELECT * FROM usuarios WHERE email_id = ?', [email_id], (err, rows, fields) => {
+//     if (!err) {
+//       res.json(rows[0]);
+//     } else {
+//       console.log(err);
+//     }
+//   });
+// });
+
 router.get('/users/get/:email_id', (req, res) => {
   const { email_id } = req.params; 
-  mysqlConnection.query('SELECT * FROM usuarios WHERE email_id = ?', [email_id], (err, rows, fields) => {
-    if (!err) {
-      res.json(rows[0]);
-    } else {
-      console.log(err);
-    }
-  });
+  sequilize
+    .query('SELECT * FROM usuarios WHERE email_id = ?',
+    { raw: true, replacements: [email_id]})
+    .then(rows => {
+      res.json(rows[0])})
+    .catch(err => {
+      console.error('ERROR:', err);
+    });
 });
 
 // DELETE An User
+// router.delete('/users/delete/:email_id', (req, res) => {
+//   const { email_id } = req.params;
+//   mysqlConnection.query('DELETE FROM usuarios WHERE email_id = ?', 
+//     { raw: true, replacements: [email_id]}).then(rows => {
+//       res.json(rows[0])
+//     .catch(err => {
+//       console.error('ERROR:', err);
+//     });  
+//   });
+// });
+
 router.delete('/users/delete/:email_id', (req, res) => {
-  const { email_id } = req.params;
-  mysqlConnection.query('DELETE FROM usuarios WHERE email_id = ?', [email_id], (err, rows, fields) => {
-    if(!err) {
-      res.json({status: 'Usuario Deleted'});
-    } else {
-      console.log(err);
-    }
-  });
+  const { email_id } = req.params; 
+  sequilize
+    .query('DELETE FROM usuarios WHERE email_id = ?',
+    { raw: true, replacements: [email_id] })
+    .then(rows => {
+      res.json(rows[0])})
+    .catch(err => {
+      console.error('ERROR:', err);
+    });
 });
 
+
 // INSERT An User
+// router.post('/users/create', (req, res) => {
+//   const {nombres, apellidos, email_id, contraseña} = req.body;
+//   console.log(req.body);
+//   const query = `
+//   INSERT INTO usuarios  
+//   VALUES (?,?,?,?,TRUE,NULL,NULL,NULL,NULL);`;
+//   mysqlConnection.query(query, [nombres, apellidos, email_id, contraseña], (err, rows, fields) => {
+//     if(!err) {
+//       res.json({status: 'User Create and Saved'});
+//     } else {
+//       console.log(err);
+//     }
+//   });
+// });
+
 router.post('/users/create', (req, res) => {
   const {nombres, apellidos, email_id, contraseña} = req.body;
-  console.log(req.body);
   const query = `
   INSERT INTO usuarios  
   VALUES (?,?,?,?,TRUE,NULL,NULL,NULL,NULL);`;
-  mysqlConnection.query(query, [nombres, apellidos, email_id, contraseña], (err, rows, fields) => {
-    if(!err) {
-      res.json({status: 'User Create and Saved'});
-    } else {
-      console.log(err);
-    }
-  });
+  sequilize
+    .query(query,
+    { raw: true, replacements: [nombres, apellidos, email_id, contraseña] })
+    .then(rows => {
+      res.json(rows[0])})
+    .catch(err => {
+      console.error('ERROR:', err);
+    });
 });
 
 // UpDate An User
+// router.put('/users/update/:email_id', (req, res) => {
+//   const { edad, carrera, semestre } = req.body;
+//   const { email_id } = req.params;
+//   const query = `
+//     UPDATE usuarios
+//       SET edad = ?, carrera= ?, semestre = ?
+//       WHERE email_id = ?;
+//   `;
+//   mysqlConnection.query(query, [edad, carrera, semestre, email_id], (err, rows, fields) => {
+//     if(!err) {
+//       res.json({status: 'User Updated'});
+//     } else {
+//       console.log(err);
+//     }
+//   });
+// });
+
 router.put('/users/update/:email_id', (req, res) => {
   const { edad, carrera, semestre } = req.body;
   const { email_id } = req.params;
@@ -67,12 +133,13 @@ router.put('/users/update/:email_id', (req, res) => {
       SET edad = ?, carrera= ?, semestre = ?
       WHERE email_id = ?;
   `;
-  mysqlConnection.query(query, [edad, carrera, semestre, email_id], (err, rows, fields) => {
-    if(!err) {
-      res.json({status: 'User Updated'});
-    } else {
-      console.log(err);
-    }
+  sequilize
+  .query(query,
+  { raw: true, replacements: [edad, carrera, semestre, email_id]})
+  .then(rows => {
+    res.json(rows[0])})
+  .catch(err => {
+    console.error('ERROR:', err);
   });
 });
 
